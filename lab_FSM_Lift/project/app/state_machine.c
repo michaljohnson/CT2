2137a3610896d6e7051ffb4d3ac454810c1df7f7
Michal Johnson <johnsmic@students.zhaw.ch>
2025-05-07 13:04:12 +0200

    Refactor state_machine.c for improved readability and structure

--- lab_FSM_Lift/project/app/state_machine.c
+++ lab_FSM_Lift/project/app/state_machine.c
@@ -20,37 +20,33 @@
 #include "action_handler.h"
 #include "timer.h"
 
-
 /* -- Macros
  * ------------------------------------------------------------------------- */
 
-#define SAFETY_DURATION      150u       // 150 * 10ms = 1.5s
-#define SIGNAL_DURATION      100u       // 100 * 10ms = 1s
+#define SAFETY_DURATION 150u // 150 * 10ms = 1.5s
+#define SIGNAL_DURATION 100u // 100 * 10ms = 1s
 
-#define TEXT_F0_OPENED       "F0_OPENED"
-#define TEXT_F0_CLOSED       "F0_CLOSED"
-#define TEXT_F1_OPENED       "F1_OPENED"
-#define TEXT_F1_CLOSED       "F1_CLOSED"
-#define TEXT_MOVING_UP       "MOVING_UP"
-#define TEXT_MOVING_DOWN     "MOVING_DOWN"
+#define TEXT_F0_OPENED "F0_OPENED"
+#define TEXT_F0_CLOSED "F0_CLOSED"
+#define TEXT_F1_OPENED "F1_OPENED"
+#define TEXT_F1_CLOSED "F1_CLOSED"
+#define TEXT_MOVING_UP "MOVING_UP"
+#define TEXT_MOVING_DOWN "MOVING_DOWN"
 
 /// STUDENTS: To be programmed
 
-
-
-
 /// END: To be programmed
-
 
 /* -- Type definitions
  * ------------------------------------------------------------------------- */
 
 // definition of FSM states
-typedef enum {
+typedef enum
+{
     /* task 4.1 */
     F0_OPENED,
     F0_CLOSED,
-    
+
     /* task 4.2 */
     F1_OPENED,
     F1_CLOSED,
@@ -59,20 +55,15 @@ typedef enum {
 
     /// STUDENTS: To be programmed
 
-
-
-
     /// END: To be programmed
-   
-} state_t;
 
+} state_t;
 
 /* Module-wide variables & constants
  * ------------------------------------------------------------------------- */
 
-// current FSM state 
+// current FSM state
 static state_t state = F0_CLOSED;
-
 
 /* Public function definitions
  * ------------------------------------------------------------------------- */
@@ -84,27 +75,39 @@ void fsm_init(void)
 {
     action_handler_init();
     ah_show_exception(NORMAL, "");
-    
+
     /* go to initial state & do initial actions */
-    
+
     /// STUDENTS: To be programmed
 
-
-
+    ah_show_state(TEXT_F0_CLOSED);
 
     /// END: To be programmed
 }
-
 
 /*
  * See header file
  */
 void fsm_handle_event(event_t event)
 {
-    /// STUDENTS: To be programmed
+    switch (state)
+    {
+    case F0_CLOSED:
+        if (event == EV_DOOR0_OPEN_REQ)
+        {
+            ah_door(DOOR_OPEN);
+            ah_show_state(TEXT_F0_OPENED);
+            state = F0_OPENED;
+        }
+        break;
 
-
-
-
-    /// END: To be programmed
+    case F0_OPENED:
+        if (event == EV_DOOR0_CLOSE_REQ)
+        {
+            ah_door(DOOR_CLOSE);
+            ah_show_state(TEXT_F0_CLOSED);
+            state = F0_CLOSED;
+        }
+        break;
+    }
 }
